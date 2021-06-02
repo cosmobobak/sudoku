65b3b4c2cd520e181a6d6759dc7da8e694501be9
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2021-06-02 01:53:42 +0100

    prettier benchmark

--- sudoku_bench.cpp
+++ sudoku_bench.cpp
@@ -16,11 +16,14 @@ int main() {
 
     SudokuBoard driver;
 
+    std::string easiest_sudoku, hardest_sudoku;
+    long long min_time = std::numeric_limits<long long>::max(), max_time = 0;
+
     std::string line;
     // Read data, line by line
     while (std::getline(sudokus, line)) {
+        line.pop_back();
         driver.set_state(line);
-        *(line.end() - 1) = ' ';
 
         auto start = std::chrono::system_clock::now();
         driver.solve_dfs();
@@ -28,7 +31,20 @@ int main() {
 
         auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
-        std::cout << line << "solved in " << std::right << std::setw(5) << time << " microseconds." << std::endl;
+        if (time > max_time) {
+            max_time = time;
+            hardest_sudoku = line;
+        }
+
+        if (time < min_time) {
+            min_time = time;
+            easiest_sudoku = line;
+        }
+
+        std::cout << line << " solved in " << std::right << std::setw(5) << time << "μs." << std::endl;
     }
+    std::cout << std::endl;
+    std::cout << "hardest sudoku " << hardest_sudoku << " took " << std::right << std::setw(5) << max_time << "μs." << std::endl;
+    std::cout << "easiest sudoku " << easiest_sudoku << " took " << std::right << std::setw(5) << min_time << "μs." << std::endl;
     return 0;
 }
