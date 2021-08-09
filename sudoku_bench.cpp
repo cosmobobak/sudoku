771ec874ff1fe0d1874b1c71ebc3b451a38f44bb
cosmobobak <cosmobobak@gmail.com>
2021-08-09 15:48:24 +0100

    better design for sudoku benchmark

--- sudoku_bench.cpp
+++ sudoku_bench.cpp
@@ -8,6 +8,18 @@
 
 #include "sudoku.hpp"
 
+const std::string BENCHMARK_FILENAME = "benchmark_set.txt";
+
+auto file_line_count(std::string filename) -> int {
+    std::ifstream file(filename);
+    int line_count = 0;
+    std::string line;
+    while (std::getline(file, line)) {
+        ++line_count;
+    }
+    return line_count;
+}
+
 int main(int argc, char* argv[]) {
     int max_sudokus_processed = 100;
     if (argc > 1) {
@@ -16,8 +28,10 @@ int main(int argc, char* argv[]) {
 
     assert(max_sudokus_processed > 0);
     
+    max_sudokus_processed = file_line_count(BENCHMARK_FILENAME) - 1;
+
     // Create an input filestream
-    std::ifstream sudokus("benchmark_set.txt");
+    std::ifstream sudokus(BENCHMARK_FILENAME);
 
     // Make sure the file is open
     if (!sudokus.is_open()) throw std::runtime_error("Could not open file");
@@ -31,7 +45,11 @@ int main(int argc, char* argv[]) {
     // Read data, line by line
     int count = 0;
     while (std::getline(sudokus, line)) {
+        printf("%d out of %d\r", count, max_sudokus_processed);
+        fflush(stdout);
+
         line.resize(81);
+        line.replace(0, 1, " ");
         std::for_each(line.begin(), line.end(), [](auto& c){ 
             if (c == '.') {
                 c = '-';
@@ -55,10 +73,11 @@ int main(int argc, char* argv[]) {
             easiest_sudoku = line;
         }
 
-        std::cout << std::right << std::setprecision(2) << ((double)count / (double)max_sudokus_processed * 100.0) << "% done.\r";
-        std::cout << line << " solved in " << std::right << std::setw(6) << time << "μs." << std::endl;
+        // std::cout << std::right << std::setprecision(2) << ((double)count / (double)max_sudokus_processed * 100.0) << "% done.\r";
+        // std::cout << line << " solved in " << std::right << std::setw(6) << time << "μs." << std::endl;
         if (count++ == max_sudokus_processed) break;
     }
+    printf("%d out of %d\n", count, max_sudokus_processed);
     std::cout << std::endl;
     std::cout << "hardest sudoku " << hardest_sudoku << " took " << std::right << std::setw(6) << max_time << "μs." << std::endl;
     std::cout << "easiest sudoku " << easiest_sudoku << " took " << std::right << std::setw(6) << min_time << "μs." << std::endl;
