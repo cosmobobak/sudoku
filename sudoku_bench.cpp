1f28db5618629598287dad78cf31c4c9cd3d3f18
cosmobobak <cosmobobak@gmail.com>
2021-08-10 20:38:32 +0100

    cleanup and testing new method

--- sudoku_bench.cpp
+++ sudoku_bench.cpp
@@ -10,6 +10,7 @@
 #include "fastfile.hpp"
 
 const char* BENCHMARK_FILENAME = "benchmark_set.txt";
+constexpr auto SUDOKU_LINE_LEN = 81;
 
 int main(int argc, char* argv[]) {
     int max_sudokus_processed = fast_count_lines(BENCHMARK_FILENAME) - 1;
@@ -31,10 +32,13 @@ int main(int argc, char* argv[]) {
     std::string easiest_sudoku, hardest_sudoku;
     long long min_time = std::numeric_limits<long long>::max(), max_time = 0;
 
-    std::string line;
-    line.resize(81);
-    // Read data, line by line
     int count = 0;
+    std::string line;
+
+    // time the whole execution
+    auto global_start = std::chrono::system_clock::now();
+
+    // Read data, line by line
     while (std::getline(sudokus, line)) {
         printf("%d out of %d\r", count, max_sudokus_processed);
         fflush(stdout);
@@ -43,19 +47,19 @@ int main(int argc, char* argv[]) {
         driver.set_state(line);
 
         auto start = std::chrono::system_clock::now();
-        driver.solve_dfs();
+        driver.solve_preproc_dfs();
         auto end = std::chrono::system_clock::now();
 
         auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
         if (time > max_time) {
             max_time = time;
-            hardest_sudoku = line;
+            hardest_sudoku = line.substr(0, SUDOKU_LINE_LEN);
         }
 
         if (time < min_time) {
             min_time = time;
-            easiest_sudoku = line;
+            easiest_sudoku = line.substr(0, SUDOKU_LINE_LEN);
         }
 
         // std::cout << std::right << std::setprecision(2) << ((double)count / (double)max_sudokus_processed * 100.0) << "% done.\r";
@@ -64,8 +68,19 @@ int main(int argc, char* argv[]) {
         count++;
     }
     printf("%d out of %d\n", count, max_sudokus_processed);
-    std::cout << std::endl;
-    std::cout << "hardest sudoku " << hardest_sudoku << " took " << std::right << std::setw(6) << max_time << "μs." << std::endl;
-    std::cout << "easiest sudoku " << easiest_sudoku << " took " << std::right << std::setw(6) << min_time << "μs." << std::endl;
+
+    auto global_end = std::chrono::system_clock::now();
+
+    auto global_time = std::chrono::duration_cast<std::chrono::microseconds>(global_end - global_start).count();
+
+    std::cout << std::endl
+              << "hardest sudoku "
+              << hardest_sudoku << " took "
+              << std::right << std::setw(6) << max_time << "μs." << std::endl
+              << "easiest sudoku "
+              << easiest_sudoku << " took "
+              << std::right << std::setw(6) << min_time << "μs." << std::endl
+              << "total time: " 
+              << std::right << std::setw(6) << global_time << "μs." << std::endl;
     return 0;
 }
